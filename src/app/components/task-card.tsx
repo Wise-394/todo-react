8c77576c661963e55673eb2428fddb3a59570108
rod <jhennroddumlao@.gmail.com>
2025-05-23 18:18:34 +0800

    card visual adjustment, body background

--- src/app/components/task-card.tsx
+++ src/app/components/task-card.tsx
@@ -1,7 +1,16 @@
 export default function TaskCard() {
+    const title = "Task Title";
+    const description = "Task Description"
     return (
-        <div className="bg-card w-full md:w-1/2 lg:w-1/4">
-            <p className="text-red-500">Task name</p>
+        <div className="flex flex-row justify-between w-full p-3 rounded bg-card md:w-1/2 lg:w-1/4">
+            <div>
+                <p className="text-white">{title}</p>
+                <p className="text-white">{description}</p>
+            </div>
+            <div>
+                <input type="checkbox" className="scale-150 bg-white border-2 border-gray-400 rounded-sm" />
+            </div>
         </div>
+
     )
 }
