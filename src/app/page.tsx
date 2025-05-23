befede1b96088308c743bb84d1a1a0e1c92a9c3d
Zed <zedcaoile@gmail.com>
2025-05-23 19:38:15 +0800

    modify header and make user data dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import TaskCard from "./components/task-card";
 
 export default function Home() {
   return (
-    <div className="p-4 sm:p-6 md:p-8 lg:p-10 bg-primary">
+    <div className="p-4 sm:p-6 md:p-8 lg:p-10 bg-gray-700">
       <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-white">Ongoing Task</p>
       <div className="flex flex-col md:flex-row gap-4">
         <TaskCard />
