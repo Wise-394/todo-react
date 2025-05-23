08c564de51a4d7a45eaf832e43d622f586993010
rod <jhennroddumlao@.gmail.com>
2025-05-23 20:00:06 +0800

    task list

--- src/app/components/task-card.tsx
+++ src/app/components/task-card.tsx
@@ -1,8 +1,11 @@
-export default function TaskCard() {
-    const title = "Task Title";
-    const description = "Task Description"
+interface Props {
+    title: String,
+    description: String
+}
+export default function TaskCard({title, description}: Props) {
+
     return (
-        <div className="flex flex-row justify-between w-full p-3 rounded bg-card md:w-1/2 lg:w-1/4">
+        <div className="flex flex-row justify-between w-full p-3 rounded-lg bg-card md:w-1/2 lg:w-1/4">
             <div>
                 <p className="text-white">{title}</p>
                 <p className="text-white">{description}</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,43 @@
 import TaskCard from "./components/task-card";
 
 export default function Home() {
+
+  const taskData = [
+    {
+      id: '1',
+      title: 'Task 1',
+      description: 'This is an example task description.'
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+      description: 'Another task with a different description.'
+    },
+    {
+      id: '3',
+      title: 'Task 3',
+      description: 'Yet another task to display in the list.'
+    },
+    {
+      id: '4',
+      title: 'Task 4',
+      description: 'This task has a unique description.'
+    },
+    {
+      id: '5',
+      title: 'Task 5',
+      description: 'Final task in the sample data array.'
+    }
+  ];
+
   return (
     <div className="p-4 sm:p-6 md:p-8 lg:p-10 bg-primary">
-      <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-white">Ongoing Task</p>
-      <div className="flex flex-col md:flex-row gap-4">
-        <TaskCard />
-
+      <p className="text-base text-white sm:text-lg md:text-xl lg:text-2xl">Ongoing Task</p>
+      <div className="flex flex-col gap-4 md:flex-row">
+      {taskData.map(task => (
+          <TaskCard key={task.id} title={task.title} description={task.description} />
+        ))}
       </div>
-
     </div>
   );
 }
