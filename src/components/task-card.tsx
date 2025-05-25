1023556a46b1e099d83d7d490094809aa84a079e
rod <jhennroddumlao@.gmail.com>
2025-05-25 21:04:16 +0800

    minor changes added onClick behaviour on task-card.tsx div

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -4,21 +4,22 @@ interface Props {
     task: Task
     handleTaskCheckbox: (id: number) => void
 }
-export default function TaskCard({task, handleTaskCheckbox}: Props) {
+export default function TaskCard({ task, handleTaskCheckbox }: Props) {
 
     const handleOnChange = () => {
         handleTaskCheckbox(task.id);
     }
 
     return (
-        <div className="flex flex-row justify-between w-full p-3 rounded-lg bg-card md:w-1/2 lg:w-1/4">
+        <div className="flex flex-row justify-between w-full p-3 rounded-lg bg-card md:w-1/2 lg:w-1/4"
+        onClick={handleOnChange}>
             <div>
                 <p className="text-white">{task.title}</p>
-               <p className={`text-white${task.isCompleted ? ' line-through' : ''}`}>{task.description}</p>
+                <p className={`text-white${task.isCompleted ? ' line-through' : ''}`}>{task.description}</p>
             </div>
             <div>
-                <input type="checkbox" className="scale-150 bg-white border-2 border-gray-400 rounded-sm" 
-                checked= {task.isCompleted} onChange={handleOnChange}/>
+                <input type="checkbox" className="scale-150 bg-white border-2 border-gray-400 rounded-sm"
+                    checked={task.isCompleted} onChange={handleOnChange} />
             </div>
         </div>
 
